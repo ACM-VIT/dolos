8757508c3e57719b9538e95c53d9e5943c0e5c85
Yash Singhal <singhalyash8080@gmail.com>
2020-10-07 02:25:16 +0530

    added env and fixed package-lock.json

--- src/database.js
+++ src/database.js
@@ -1,12 +1,15 @@
 var mysql = require('mysql');
+const dotenv = require("dotenv");
+dotenv.config();
+
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword"
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'yourUser',
+  password: process.env.DB_PASS || 'yourPass'
 });
 
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
-});
+  console.log("Connected!")
+})
