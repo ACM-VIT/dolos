344d37426e9f29f270be823ae03cd3eb0861052d
shadabshaikh0 <shadab.shaikh@whitehatjr.com>
2020-10-06 23:24:57 +0530

    Added env variables for db connectivity

--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
 });
 
 con.connect(function(err) {
