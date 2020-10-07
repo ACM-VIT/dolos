174b000e835661a0b5e2f2d79dfa08acabf4951a
Mayank <nitb.scholar@gmail.com>
2020-10-07 22:14:49 +0530

    adding docker with env, also added retry to mysql

--- src/database.js
+++ src/database.js
@@ -1,12 +1,27 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword"
+  host: process.env.MYSQL_HOST || "localhost",
+  user: process.env.MYSQL_USER || "yourusername",
+  password: process.env.MYSQL_PASSWORD || "yourpassword",
+  database: process.env.MYSQL_DB || "yourdatabase",
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+
+var connectMysql = function() {
+  con.connect(function(err) {
+    if (err) {
+      console.log(err);
+      
+      // retry added after 10 seconds to avoid failure of mysql
+      setTimeout(function() {
+        connectMysql();
+      }, 10000);
+    }
+    console.log("Connected!");
+  });
+};
+
+connectMysql();
+
+
