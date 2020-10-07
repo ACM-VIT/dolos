100712beb5ae8c314e4a14dec5304abdd0fd841a
shadabshaikh0 <shadab.shaikh@whitehatjr.com>
2020-10-07 11:34:36 +0530

    Added default varibles for db connectivity

--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'root'
 });
 
 con.connect(function(err) {
