09f6223a75115e11a74d22ae67d77e916f8ac1f9
Waing La Min Lwin <37790271+archonn@users.noreply.github.com>
2020-10-07 17:08:46 +0630

    Added .env.example + Added test script for server. (#35)
    * Exported express app
    
    * Added .env.example
    
    * Add test to test server is working

--- src/tests/sampleTest.test.js
+++ src/tests/sampleTest.test.js
@@ -1,5 +1,5 @@
 describe('Sample Test', () => {
-    it('should test that true === true', () => {
-      expect(true).toBe(true)
-    })
-  })
+  it('should test that true === true', () => {
+    expect(true).toBe(true);
+  });
+});
