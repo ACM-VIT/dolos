b4f5f2b1c05c9728855bfdbbd0813b7b393e44bf
singhalyash8080 <60805305+singhalyash8080@users.noreply.github.com>
2020-10-08 18:13:10 +0530

    Update database.js

--- src/database.js
+++ src/database.js
@@ -6,7 +6,8 @@ dotenv.config();
 var con = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'yourUser',
-  password: process.env.DB_PASS || 'yourPass'
+  password: process.env.DB_PASS || 'yourPass',
+  port: process.env.DB_PORT || 3000
 });
 
 con.connect(function(err) {
