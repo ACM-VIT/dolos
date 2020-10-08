9802cead1f7a4c6e009413e7dd1405df6b23a1c0
vj-abishek <abigosearch@gmail.com>
2020-10-08 09:46:30 +0530

    Ignore linting for .test.js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module",
   },
+  eslintIgnore: ["*.test.js"],
   rules: {
     "prettier/prettier": "error",
     "class-methods-use-this": "off",
