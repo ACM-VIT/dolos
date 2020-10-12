a14ec955375a85fd272e539887c56f9552ad4425
singhalyash8080 <60805305+singhalyash8080@users.noreply.github.com>
2020-10-12 08:46:39 +0530

    load env configs for dbms connectivity (#51)
    Co-authored-by: Yash Singhal <singhalyash8080@gmail.com>

--- src/database.js
+++ src/database.js
@@ -1,12 +1,18 @@
-const mysql = require("mysql");
 
-const con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword",
-});
+var mysql = require('mysql');
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+var con = mysql.createConnection({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'yourUser',
+  password: process.env.DB_PASS || 'yourPass',
+  port: process.env.DB_PORT || 3000
+
 
 con.connect(function (err) {
   if (err) throw err;
-  console.log("Connected!");
-});
+  console.log("Connected!")
+})
+
