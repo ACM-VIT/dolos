a436b8825b66a7b2d54b60c81d36c83637753a68
Shadab Majid Shaikh <sshadabshaikh7703@gmail.com>
2020-10-13 16:35:01 +0530

    Added env variables for db connectivity (#44)
    * Added env variables for db connectivity
    
    * Added default varibles for db connectivity
    
    * Added .env.example
    
    Co-authored-by: shadabshaikh0 <shadab.shaikh@whitehatjr.com>

--- src/database.js
+++ src/database.js
@@ -7,9 +7,8 @@ dotenv.config();
 var con = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'yourUser',
-  password: process.env.DB_PASS || 'yourPass',
-  port: process.env.DB_PORT || 3000
-
+  password: process.env.DB_PASS || 'yourPass'
+});
 
 con.connect(function (err) {
   if (err) throw err;
