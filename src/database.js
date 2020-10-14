fe6e3a3563ad77d0ec0569235a2240a60711ce63
Anat Dagan <anadt@ironsrc.com>
2020-10-14 10:43:23 +0300

    Fix: sync database.js with production

--- src/database.js
+++ src/database.js
@@ -7,12 +7,10 @@ dotenv.config();
 var con = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'yourUser',
-  password: process.env.DB_PASS || 'yourPass',
-  port: process.env.DB_PORT || 3000});
-
+  password: process.env.DB_PASS || 'yourPass'
+});
 
 con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!")
 })
-
