48f508b2a90f4f0dd20f6eb00c0d0397fb393b20
Henrique Paulo Bonfim <40275173+hpbonfim@users.noreply.github.com>
2020-10-16 00:25:26 -0300

    add .env.database for database and docker for development (#50)
    * add .env.database for database and docker for development
    
    * Update database.js
    
    Add fixed variables to use
    
    * Delete package-lock.json
    
    * mv .env.database to .env.example
    
    * add package-lock.json to pass the CI test
    
    Co-authored-by: Yash Kumar Verma <yk.verma2000@gmail.com>

--- src/database.js
+++ src/database.js
@@ -1,16 +1,28 @@
+const mysql = require('mysql');
+require('dotenv').config()
 
-var mysql = require('mysql');
-const dotenv = require("dotenv");
-dotenv.config();
+const config = process.env.NODE_ENV == "docker" ?
+  {
+    database: process.env.DOCKER_DB_DATABASE,
+    port: process.env.DOCKER_DB_PORT,
+    host: process.env.DOCKER_DB_HOST,
+    user: process.env.DOCKER_DB_USER,
+    password: process.env.DOCKER_DB_PASSWORD
+  } : {
+    database: process.env.DB_DATABASE || "db",
+    port: process.env.DB_PORT || "3306",
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "user",
+    password: process.env.DB_PASSWORD || "pass"
+  }
 
+const connection = new mysql.createConnection(config)
 
-var con = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'yourUser',
-  password: process.env.DB_PASS || 'yourPass'
+connection.connect((err) => {
+  if (err)
+    console.log('Database not connected! : ' + JSON.stringify(err, undefined, 2));
+  else
+    console.log('Database is connected!');
 });
 
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!")
-})
+module.exports = connection
